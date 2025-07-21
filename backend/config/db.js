ff9cfa7e6b1a30a3ee8f6abb9571138eb7ab3662
vishwa-teja-b <vishwateja2k4@gmail.com>
2025-07-21 16:50:12 +0530

    changes made

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });;
         console.log("MongoDB connected successfully");
     }catch(error){
         console.log(error.message);
